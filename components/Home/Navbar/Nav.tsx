67d9d4ecfabcc077164f1d9d3296dadc90ef36ca
Arcane-Boy <97098898+Ghostboy8@users.noreply.github.com>
2025-04-18 21:39:03 +0000

    Update Nav.tsx
    nav bg

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -26,7 +26,7 @@ const Nav = ({ openNav }: Props) => {
   }, []);
 
   return (
-    <div className={`fixed ${navBg ? 'bg-white  shadow-md' : 'fixed'} w-full transition-all duration-200 h-[12vh] z-[1000]`}>
+    <div className={`fixed ${navBg ? 'bg-white  shadow-md' : 'fixed bg-white  shadow-md'} w-full transition-all duration-200 h-[12vh] z-[1000]`}>
       <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
 
         {/* Logo réduit avec espacement supérieur */}
